7c193c2d72664c575cfcf247074a891450969e5f
Simon <simon.eismann@uni-wuerzburg.de>
2020-02-17 10:59:13 +0100

    current state + initial .gitignore

---
+++ Data.py
@@ -0,0 +1,156 @@
+import os
+import numpy as np
+import re
+from io import StringIO
+import pandas as pd
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_colwidth', -1)
+pd.options.display.width = None
+basefolder = "C:\\Users\\Simon\\Desktop\\mowgli-ml-data\\results\\scalability-ycsb-write\\openstack\\cassandra"
+
+
+class Dataset:
+    exps = False
+
+    def __init__(self, exps):
+        self.exps = exps
+
+    def calculate_robust_metric(self, calculate_robust_metric_function):
+        data_set = []
+        for exp in self.exps:
+            data_set.append(exp.calculate_robust_metric(calculate_robust_metric_function))
+        df = pd.DataFrame(data_set)
+        df = df.apply(pd.to_numeric, errors='ignore')
+        #df['feature/clustersize'] = df['feature/clustersize'].astype(int)
+        #df['feature/replicationfactor'] = df['feature/replicationfactor'].astype(int)
+        #df['target/throughput'] = df['target/throughput'].astype(int)
+        return df
+
+
+class Experiment:
+    features = False
+    measurement_values = False
+    configuration = False
+
+    def __init__(self, configuration):
+        self.configuration = configuration
+        folders = os.listdir(basefolder + "\\" + configuration)
+        if "plots" in folders:
+            folders.remove("plots")
+        if len(folders) > 1:
+            self.features = self.extract_features(configuration)
+            path = basefolder + "\\" + configuration
+            folders = os.listdir(path)
+            folders = [path + "\\" + fol for fol in folders]
+            self.measurement_values = []
+            for repPath in folders:
+                if not (repPath.__contains__("plots") | repPath.__contains__("archiv")):
+                    if os.path.isfile(repPath + "\\data\\load.txt"):
+                        with open(repPath + "\\data\\load.txt", encoding='utf8') as f:
+                            text = f.read().strip()
+                            text = re.sub(r'^\[OVERALL\].*\n?', '', text, flags=re.MULTILINE)
+                            text = re.sub(r'^\[INSERT\].*\n?', '', text, flags=re.MULTILINE)
+                            resps = pd.read_csv(StringIO(text), sep=";", names=['Time', 'Throughput', 'Latency', 'Garbage'])
+                            metrics = {'throughputdata': np.asarray(resps['Throughput']), 'latencydata': np.asarray(resps['Latency'])}
+                            if len(metrics['throughputdata']) == 0:
+                                print("[WARN] Empty load.txt file: ", repPath)
+                            else:
+                                self.measurement_values.append(metrics['throughputdata'])
+                    else:
+                        print("[WARN] No load.txt file:", repPath)
+        else:
+            raise Exception('Invalid Experiment: ' + configuration)
+
+    def calculate_robust_metric(self, calculate_robust_metric_function):
+        robust_metrics = []
+        for raw_measurements in self.measurement_values:
+            robust_metrics.append(calculate_robust_metric_function(raw_measurements))
+        return {**{'configuration': self.configuration}, **self.features, **{'target/throughput': robust_metrics}}
+
+    def extract_features(self, folder_name):
+        list_of_features = folder_name.split("_")
+        vm_size = list_of_features[0][3:]
+        cores, memory = self.extract_vm_specs(vm_size)
+        cluster_size = list_of_features[1].split("-")[1]
+        client_consistency = self.format_client_consistency(list_of_features[3].split("-")[1])
+        replication_factor = list_of_features[2].split("-")[1]
+        feats = {'feature/clustersize': cluster_size, 'feature/replicationfactor': replication_factor, 'feature/clientconsistency': client_consistency, 'feature/cores': cores, 'feature/memory': memory}
+        return feats
+
+    def extract_vm_specs(self, vm_size):
+        if vm_size == "tiny":
+            return 1, 2
+        if vm_size == "small":
+            return 2, 4
+        if vm_size == "medium":
+            return 4, 8
+        if vm_size == "large-memory":
+            return 4, 12
+        if vm_size == "large-cpu":
+            return 6, 8
+        raise ValueError("Unknown VM size: " + vm_size)
+
+    def format_client_consistency(self, client_consistency):
+        if client_consistency == "one":
+            return 1
+        if client_consistency == "two":
+            return 2
+        if client_consistency == "three":
+            return 3
+        raise ValueError("Unknown client consistency: " + client_consistency)
+
+
+def is_valid_exp(configuration):
+    folders = os.listdir(basefolder + "\\" + configuration)
+    if "plots" in folders:
+        folders.remove("plots")
+    if "archiv" in folders:
+        folders.remove("archiv")
+    if len(folders) > 1:
+        if len(folders) != 10:
+            print("[WARN] Unexpected number of experiment repetitions detected for " + configuration + ": ", len(folders))
+    return len(folders) > 1
+
+
+def load_data_set():
+    exps = []
+    for config in os.listdir(basefolder):
+        if is_valid_exp(config):
+            if config != "vm-large-memory_cs-7_rf-3_cc-two" and config != "vm-medium_cs-3_rf-3_cc-one" and config != "vm-tiny_cs-9_rf-2_cc-two":
+                exp = Experiment(config)
+                # Remove me later
+                if len(exp.measurement_values) != 0:
+                    exps.append(exp)
+    return Dataset(exps)
+
+
+def load_small_vm_data_set():
+    exps = []
+    for config in os.listdir(basefolder):
+        if is_valid_exp(config):
+            if config != "vm-large-memory_cs-7_rf-3_cc-two" and config != "vm-medium_cs-3_rf-3_cc-one" and config != "vm-tiny_cs-9_rf-2_cc-two":
+                exp = Experiment(config)
+                # Remove me later
+                if exp.features['feature/cores'] == 2:
+                    if len(exp.measurement_values) != 0:
+                        exps.append(exp)
+    return Dataset(exps)
+
+
+def load_large_vm_data_set():
+    exps = []
+    for config in os.listdir(basefolder):
+        if is_valid_exp(config):
+            if config != "vm-large-memory_cs-7_rf-3_cc-two" and config != "vm-medium_cs-3_rf-3_cc-one" and config != "vm-tiny_cs-9_rf-2_cc-two":
+                exp = Experiment(config)
+                # Remove me later
+                if exp.features['feature/cores'] == 6:
+                    if len(exp.measurement_values) != 0:
+                        exps.append(exp)
+    return Dataset(exps)
+
+if __name__ == "__main__":
+    ds = load_data_set()
+    print(ds.calculate_robust_metric(np.mean).dtypes)
---
+++ PlotMeasurements.py
@@ -0,0 +1,138 @@
+import numpy as np
+import Analysis2.Data as Data
+from scipy import stats
+from sklearn.ensemble import RandomForestRegressor
+import boltons.statsutils as su
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import NearestNeighbors
+from pyearth import Earth
+from sklearn.neural_network import MLPRegressor
+
+from sklearn import linear_model
+
+from sklearn import tree
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_palette(sns.color_palette("hls", 20))
+
+
+distplots = False
+prediction = True
+
+
+def calculateHL(l_input):
+    l_avgs = []
+    k = 0
+    j = 0
+    while k < len(l_input):
+        while j < len(l_input):
+            l_avgs.append(np.mean([l_input[k], l_input[j]]))
+            j = j + 1
+        k = k + 1
+        j = k
+    return np.median(l_avgs)
+
+
+if __name__ == "__main__":
+    datasets = [('smallVM', Data.load_small_vm_data_set()),
+                ('largeVM', Data.load_large_vm_data_set()),
+                ('smallAndLargeVM', Data.load_data_set())
+                ]
+    approaches = [('LinReg', linear_model.LinearRegression()),
+                  ('HuberRegressor', linear_model.HuberRegressor(epsilon=100)),
+                  ('Ridge', linear_model.Ridge()),
+                  ('ElasticNet', linear_model.ElasticNet()),
+                  ('BayesianRidge', linear_model.BayesianRidge()),
+                  ('MARS', Earth()),
+                  ('RandomForest', RandomForestRegressor(n_estimators=10, random_state=42)),
+                  ('SVR', linear_model.SGDRegressor(max_iter=1000, tol=1e-3)),
+               ]
+    metrics = [('Mean', np.mean),
+               #('Median', np.median),
+               #('Max', np.max),
+               #('Min', np.min),
+               ('95th percentile', lambda x: np.percentile(x, 95))
+               #('90th percentile', lambda x: np.percentile(x, 90)),
+               #('80th percentile', lambda x: np.percentile(x, 80)),
+               #('70th percentile', lambda x: np.percentile(x, 70)),
+               #('Trimmed(5%) mean', lambda x: stats.trim_mean(x, 0.05)),
+               #('Trimmed(10%) mean', lambda x: stats.trim_mean(x, 0.1)),
+               #('Trimmed(20%) mean', lambda x: stats.trim_mean(x, 0.2)),
+               #('Trimmed(30%) mean', lambda x: stats.trim_mean(x, 0.3)),
+               #('Winzorized(5%) mean', lambda x: np.mean(stats.mstats.winsorize(x, [0.05, 0.05]))),
+               #('Winzorized(10%) mean', lambda x: np.mean(stats.mstats.winsorize(x, [0.1, 0.1]))),
+               #('Winzorized(20%) mean', lambda x: np.mean(stats.mstats.winsorize(x, [0.2, 0.2]))),
+               #('Winzorized(30%) mean', lambda x: np.mean(stats.mstats.winsorize(x, [0.3, 0.3]))),
+               #('Trimean', lambda x: su.Stats(x).trimean),
+               #('Hodges-Lehmann', calculateHL)
+               ]
+
+    # Plot
+    if distplots:
+        # Load Data
+        ds = Data.load_data_set()
+
+        for metric in metrics:
+            df = ds.calculate_robust_metric(metric[1])
+            all_tps = []
+            all_configs = []
+            for index, row in df.iterrows():
+                for value in row['target/throughput']:
+                    all_tps.append(value)
+                    all_configs.append(row['configuration'])
+
+            plt.figure(figsize=(30, 15))
+            plt.scatter(all_configs, all_tps, label="Measurements")
+            plt.scatter(df['configuration'], df['target/throughput'].apply(np.mean), label="Mean")
+            plt.scatter(df['configuration'], df['target/throughput'].apply(np.median), label="Median")
+            for i in range(0, 58, 2):
+                plt.axvspan(i + 0.5, i+1.5, facecolor='0.2', alpha=0.3)
+            plt.xticks(rotation=90, fontsize=20)
+            plt.yticks(fontsize=20)
+            plt.xlabel("Configuration", fontsize=20)
+            plt.ylabel(metric[0] + "Throughput", fontsize=20)
+            plt.title(metric[0] + " Throughput", fontsize=26)
+            plt.xlim(-0.5, 57.5)
+            plt.legend(fontsize=20)
+            plt.tight_layout()
+            plt.savefig("distributionplots/" + metric[0] + ".png")
+            print("Finished file " + metric[0] + ".png")
+
+    # Regression
+    if prediction:
+        for approach in approaches:
+            for dataset in datasets:
+                plt.figure(figsize=(30, 15))
+                for metric in metrics:
+                    df = dataset[1].calculate_robust_metric(metric[1])
+                    y = np.asarray(df['target/throughput'].apply(np.median))
+                    x = np.asarray(df.drop(columns=['configuration', 'target/throughput']))
+                    orderedMapes = []
+
+
+                    for i in range(1, len(y)):
+                        mapes = []
+                        for j in range(1, 50):
+                            xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size=(len(y)-i)/len(y), random_state=j+20)
+                            model = approach[1]
+                            model.fit(xTrain, yTrain)
+                            yPred = model.predict(xTest)
+                            errors = abs(yPred - np.asarray(yTest))
+                            mape = 100 * (errors / np.asarray(yTest))
+                            mapes.append(np.mean(mape))
+                        orderedMapes.append(np.mean(mapes))
+
+                    if metric[0] != 'Min':
+                        plt.plot(range(1, len(y)), orderedMapes, label=metric[0])
+
+                plt.ylabel("MAPE [%]", fontsize=24)
+                plt.xlabel("Trainingset size", fontsize=24)
+                plt.title(approach[0] + "_" + dataset[0], fontsize=28)
+                plt.legend(fontsize=20, ncol=3)
+                plt.xticks(fontsize=24)
+                plt.yticks(fontsize=24)
+                plt.xlim(1, len(y)-1)
+                plt.ylim(0, 30)
+                plt.savefig('predictionaccuracy_' + approach[0] + "_" + dataset[0] + '.png')
+                plt.close()
+                print(approach[0], dataset[0])
